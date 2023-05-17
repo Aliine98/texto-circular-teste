b5bfd0a896f0cdf9386fd191153efdf5f828e3bb
Aliine98 <90913523+Aliine98@users.noreply.github.com>
2023-05-17 16:53:58 -0300

    ♻ refactor: change range event to input

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const range = document.querySelector('.padding-range');
 
-range.addEventListener('change', () => {
+range.addEventListener('input', () => {
   const circularText = document.querySelector('.circle-container');
   const span = document.querySelector('.range-value').innerHTML = `${range.value}px`;
   circularText.style.setProperty('--padding', `${range.value}px`);
